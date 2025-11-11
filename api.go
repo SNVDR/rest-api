6ef50f6451f003da27efeb8d93275b09e9852b83
SNVDR <serhii.nevedrov@gmail.com>
2025-11-11 12:02:44 +0200

    prototype setup

---
+++ api.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	addr  string
+	store Store
+}
+
+// Just constructor
+func NewAPIServer(addr string, store Store) *APIServer {
+	return &APIServer{addr: addr, store: store}
+}
+
+func (s *APIServer) Serve() {
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	//Register our services
+
+	tasksService := NewTasksService(s.store)
+	tasksService.RegisterRoutes(router)
+
+	log.Println("Starting the API server at:", s.addr)
+
+	log.Fatal(http.ListenAndServe(s.addr, subrouter))
+}
